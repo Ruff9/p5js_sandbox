656e1e32621d28b511823df75ef8b9713e0f648c
remy <remy.maucourt@yahoo.fr>
2020-03-21 17:26:32 +0100

    setup active zone

--- js/colors_2.js
+++ js/colors_2.js
@@ -1,20 +1,27 @@
-let table;
+let table, origin;
 let black, white;
 
-const baseSize = 95;
+const baseSize = 65;
 const baseframeRate = 15;
+const density = 1;
+const activeAreaSize = 10;
 
 function setup() {
   frameRate(baseframeRate);
   createCanvas(windowWidth, windowHeight);
   black = color(0);
-  white = color(255);
+
+  background(black);
 
   table = new Table();
+
+  origin = {x: floor(table.columns/2), y: floor(table.rows/2)};
 
   for ( let i = 0; i < table.columns; i++ ) {
     for ( let j = 0; j < table.rows; j++ ) {
       const cell = new Cell({x: i, y: j});
+
+      if (cell.isInActiveArea(origin)) { cell.active = true; };
 
       table.cells.push(cell);
       cell.display();
@@ -23,7 +30,9 @@ function setup() {
 }
 
 function draw() {
-  table.randomCell().colorize();
+  for (step = 0; step < density; step++) {
+    table.randomActiveCell().colorize();
+  }
 }
 
 const Table = function() {
@@ -36,26 +45,33 @@ Table.prototype.randomCell = function() {
   return this.cells[floor(random() * this.cells.length)];
 };
 
+Table.prototype.randomActiveCell = function() {
+  const activeCells = this.cells.filter(cell => cell.active == true);
+  return activeCells[floor(random() * activeCells.length)];
+};
+
 const Cell = function(position) {
   this.position = position;
   this.bkgColor = black;
-  this.strokeColor = black;
   this.width = width/table.columns;
   this.height = height/table.rows;
+  this.active = false;
+  this.live = false;
 };
 
 Cell.prototype.display = function() {
   fill(this.bkgColor);
-  stroke(this.strokeColor);
+  noStroke();
 
   rect(this.position.x * this.width,
        this.position.y * this.height,
-       this.width + 1,
-       this.height + 1);
+       this.width,
+       this.height);
 };
 
 Cell.prototype.colorize = function() {
   this.bkgColor = randomGreenColor();
+  this.live = true;
 
   this.display();
   this.fade();
@@ -63,19 +79,26 @@ Cell.prototype.colorize = function() {
 
 Cell.prototype.fade = function() {
   const fading = () => {
+    // const currentAlpha = this.bkgColor._getAlpha()
+
+    // if (currentAlpha <= 0) { stopFading(); }
+
     const brightnness = this.bkgColor._array.slice(0, 2)
                                             .reduce((a, b) => a + b, 0)
 
+
     if (brightnness < 0.05) { stopFading(); }
-
     const newColor = lerpColor(this.bkgColor, black, 0.1);
-
     this.bkgColor = newColor;
+
+    // this.bkgColor.setAlpha(currentAlpha - 10)
+    // console.log(this.bkgColor._getAlpha())
     this.display();
   }
 
   const stopFading = () => {
     this.bkgColor = black;
+    this.live = false;
     this.display();
     clearInterval(fadingProcess);
   }
@@ -83,10 +106,17 @@ Cell.prototype.fade = function() {
   const fadingProcess = setInterval(fading, 100);
 }
 
+Cell.prototype.isInActiveArea = function(origin) {
+  var distance = sqrt(sq(this.position.x - origin.x) +
+                      sq(this.position.y - origin.y));
+
+  return distance < activeAreaSize;
+};
+
 function randomBlueColor() {
-  return color(random(200, 220), random(200, 220), 255);
+  return color(random(150, 220), random(150, 220), 255);
 }
 
 function randomGreenColor() {
-  return color(random(0, 200), 255,random(0, 200));
+  return color(random(150, 220), 255,random(150, 220));
 }
